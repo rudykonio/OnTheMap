def9eacb7441901ad6927e6ce36c8cb93b5f9a4b
Rodion Konioshko <rudykonio@gmail.com>
2020-03-03 15:41:03 +0200

    Log In screen UI

--- OnTheMap/controller/LoginViewController.swift
+++ OnTheMap/controller/LoginViewController.swift
@@ -9,11 +9,8 @@
 import Foundation
 import UIKit
 class LoginViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
