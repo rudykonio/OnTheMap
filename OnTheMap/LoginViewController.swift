2ad907853e7451001ed1528738f7f3edd31e1e54
Rodion Konioshko <rudykonio@gmail.com>
2020-03-03 12:41:01 +0200

    Initial Commit

---
+++ OnTheMap/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  OnTheMap
+//
+//  Created by Rodion Konioshko on 03/03/2020.
+//  Copyright © 2020 Rodion Konioshko. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
